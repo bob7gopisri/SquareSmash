69975c52a31ac16c9f0fa23550fc3581df73af92
bob7gopisri <bob7gopisri@gmail.com>
2016-04-04 15:59:50 +0530

    created db users

--- routes/play.js
+++ routes/play.js
@@ -5,13 +5,15 @@ var express = require('express');
 var util = require('../config/util.js');
 var router = express.Router();
 
-router.get('/', function(req, res) {
-    res.render('play', {
-        title: 'Chess Hub - Game',
-        user: req.user,
-        isPlayPage: true
-    });
-});
+//router.get('/', function(req, res) {
+//    res.render('play', {
+//        title: 'Chess Hub - Game',
+//        user: req.user,
+//        isPlayPage: true
+//    });
+//});
+
+router.get('/play/:id', function (req, resp) {
 
 router.post('/', function(req, res) {
     var side = req.body.side;
