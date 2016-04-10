81f42d00494fac60b64c8f2574df0da4af65ea6d
bob7gopisri <bob7gopisri@gmail.com>
2016-04-10 19:02:47 +0530

    Grid

--- routes/game.js
+++ routes/game.js
@@ -6,24 +6,17 @@ var game_model = models.game;
 
 router.get('/:permalink', function(req, res, next) {
     var permalink = req.params.permalink;
-    var num_rows = 3;
-    var num_columns = 3;
-    var max_players = 2;
 
     game_model.findOne({where: {permalink: permalink}}).then(function (game) {
         console.log("where am I");
         console.log(game);
         if (game !== null) {
-            console.log("Did I come to IFFER");
-            console.log(game.num_rows);
-            console.log(game.num_columns);
-            console.log(game.max_players);
-            console.log("how am I");
             req.flash('error', 'We have already an game name: ' + game);
             res.render('game', { title: 'SquareSmash',
                 num_rows: game.num_rows,
                 num_columns: game.num_columns,
                 max_players: game.max_players});
+
         } else { // no game found
             req.flash('error', 'No game found at ' + permalink);
             res.redirect('/play');
